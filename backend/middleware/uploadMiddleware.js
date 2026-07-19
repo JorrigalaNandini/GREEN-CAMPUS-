4a78ba4a54b98b2a259adfc52074e338a13ae777
JorrigalaNandini <nandinijorrigala1177@gmail.com>
2026-07-19 22:48:49 +0530

    Fix uploads folder creation on server

--- backend/middleware/uploadMiddleware.js
+++ backend/middleware/uploadMiddleware.js
@@ -1,9 +1,15 @@
-
 const multer = require("multer");
+const fs = require("fs");
+
+const uploadDir = "uploads/";
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, "uploads/");
+    cb(null, uploadDir);
   },
 
   filename: function (req, file, cb) {
